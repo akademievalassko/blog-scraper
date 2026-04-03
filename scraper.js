7ec2a3a8a1f4a7451412999afb37930c4f73087b
akademievalassko <info@czpvk.cz>
2026-04-03 09:16:15 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,29 +1,27 @@
+import fetch from "node-fetch";
+import * as cheerio from "cheerio";
+import fs from "fs";
+
 async function scrape() {
   const url = "https://www.akademievalassko.net/blog/";
-
   const response = await fetch(url);
   const html = await response.text();
 
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, "text/html");
+  const $ = cheerio.load(html);
 
-  const articles = [...doc.querySelectorAll("article.item")].map(item => {
-    return {
-      title: item.querySelector(".item-head h2")?.innerText.trim() || "",
-      link: item.querySelector("a")?.href || "",
-      date: item.querySelector(".item-date")?.innerText.trim() || "",
-      perex: item.querySelector(".item-perex p")?.innerText.trim() || "",
-      image: item.querySelector(".item-media img")?.src || ""
-    };
+  const articles = [];
+
+  $("article.item").each((i, el) => {
+    const title = $(el).find(".item-head h2").text().trim();
+    const link = $(el).find("a").attr("href");
+    const date = $(el).find(".item-date").text().trim();
+    const perex = $(el).find(".item-perex p").text().trim();
+    const image = $(el).find(".item-media img").attr("src");
+
+    articles.push({ title, link, date, perex, image });
   });
 
-  return articles;
+  fs.writeFileSync("data.json", JSON.stringify(articles, null, 2));
 }
 
-scrape().then(data => {
-  const json = JSON.stringify(data, null, 2);
-  const blob = new Blob([json], { type: "application/json" });
-
-  // GitHub Pages neumí dynamické generování,
-  // takže data.json vytvoříme ručně (viz další krok)
-});
+scrape();
